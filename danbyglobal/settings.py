d7a574dbc2df0d4cbf50b54bff36783c6d6a876d
Haruna Yakubu <harunayakubuu@gmail.com>
2023-02-06 21:38:14 +0100

    settings file updated

--- danbyglobal/settings.py
+++ danbyglobal/settings.py
@@ -94,12 +94,12 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 
 # Database Config
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
@@ -116,13 +116,13 @@ DATABASES = {
 
 
 # if DEBUG:
-# import dj_database_url
+import dj_database_url
 
-# DATABASE_URL = config('DATABASE_URL')
+DATABASE_URL = config('DATABASE_URL')
 
-# DATABASES = {
-#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-# }
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
 
 
 # Password validation
