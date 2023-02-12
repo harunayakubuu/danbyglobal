e2450ab3d4d1bfb5e08d54b201bcfc369b6e18b4
Haruna Yakubu <harunayakubuu@gmail.com>
2023-02-12 10:47:10 +0100

    updated full_name

--- accounts/models.py
+++ accounts/models.py
@@ -24,7 +24,7 @@ class Account(AbstractUser):
     def get_full_name(self):
         #Return the first_name plus the last_name, with a space in between.
         if self.middle_name:
-            full_name = '%s %s %s' %(self.first_name, self.middle_name, self.last_name)
+            full_name = '%s %s %s' %(self.first_name, self.last_name, self.middle_name)
         else:
             full_name = '%s %s' %(self.first_name, self.last_name)
 
